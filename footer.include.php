1f6f7a7454cc9352a0d1ca1400c21430dc1ec2f4
Robke22 <robrijken_92@hotmail.com>
2014-04-08 11:06:37 +0200

    Html & css voor front

---
+++ footer.include.php
@@ -0,0 +1,16 @@
+<div id="footerOuter">
+	<div id="footerInner">
+		<div id="copyright">
+			<p>Copyright &copy; 2014 - <a href="index.php">myResto.be</a></p>
+			<p>Alle rechten voorbehouden</p>
+		</div>
+		<div id="sitemap">
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Registreren</a></li>
+				<li><a href="#">Inloggen</a></li>
+			</ul>
+			<a href="#" id="logo_footer">myResto</a>
+		</div>
+	</div><!--end footerInner-->
+</div><!--end footerOuter-->
---
+++ nav.include.php
@@ -0,0 +1,8 @@
+<nav>
+	<a href="index.php" id="logo">myResto</a>
+	<ul>
+		<li><a href="index.php">Home</a></li>
+		<li><a href="#">Registreren</a></li>
+		<li><a href="#" class="login shadow">Inloggen</a></li>
+	</ul>
+</nav>
